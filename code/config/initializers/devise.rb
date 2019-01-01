139cc4366ec94b9c6925c10aeaade5ee9c6ba468
Hiro Koma <komahirokazu@gmail.com>
2019-01-01 17:55:31 +0900

    Update devise.rb

--- code/config/initializers/devise.rb
+++ code/config/initializers/devise.rb
@@ -20,7 +20,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = ENV['RAILS_APP_KARAMI_EMAIL_FROM_ADDRESS']
+  config.mailer_sender = ENV['RAILS_APP_@@@_EMAIL_FROM_ADDRESS']
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
